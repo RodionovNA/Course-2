f24e56c50c57a7dc28a498788582ce26a44d672c
Nikita <notname.nam@gmail.com>
2025-02-15 19:35:01 +0300

    num2

--- PythonCode/task15.02.py
+++ PythonCode/task15.02.py
@@ -14,3 +14,13 @@
 #     print("Сумма = ", end = ' ')
 #     print(a + b)
 
+
+# print("*" * 7)
+# for i in range(1, 4):
+#     if i == 3:
+#         print(" " * i + "*")
+#     else:
+#         print(" " * i + "*" + " " * (7 - 2 - i * 2) + "*")
+# for i in range(2, 0, -1):
+#     print(" " * i + "*" + " " * (7 - 2 - i * 2) + "*")
+# print("*" * 7)
