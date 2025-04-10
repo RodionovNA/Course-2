bb4d8996bb8c69deb8ad65b010ab596c47327967
Nikita <notname.nam@gmail.com>
2025-04-10 23:57:14 +0300

    10.04

---
+++ alg_dejkstra.py
@@ -0,0 +1,16 @@
+import sys
+
+
+# class Graph:
+#     def __init__(self, nodes, init_graph):
+#         self.nodes = nodes
+#         self.graph = self.construct_graph(nodes, init_graph)
+    
+#     def construct_graph(self, nodes, init_graph):
+#         graph = {}
+#         for node in nodes:
+#             graph[node] = {}
+
+#         graph.update(init_graph)
+
+print("sd")
--- codeforce.py
+++
@@ -1,6 +0,0 @@
-# def dijkstra(s):
-#     for 
-n = 4
-m = 5
-G = [[0 for _ in range(m)] for _ in range(n)]
-print(G)
